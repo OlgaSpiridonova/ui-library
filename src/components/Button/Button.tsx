10663df8381023d07b3cae8d4d89c9f9a7cdfead
OlgaSpiridonova <o.spiridonova@tg.dunice.net>
2025-07-04 17:51:54 +0300

    fix after review

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,6 @@ import clsx from 'clsx';
 
 export const Button = ({ variant = "contained", ...props}: ButtonProps & { variant?: "contained" | "outlined" | "light"}) => {
     return (
-        <HeadlessButton className={clsx(styles.button, {
-            [styles.contained]: variant === 'contained',
-            [styles.outlined]: variant === 'outlined',
-            [styles.light]: variant === 'light',
-        })} {...props} />
+        <HeadlessButton className={clsx(styles.button, styles[variant])} {...props} />
     )
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,9 +4,6 @@ import clsx from 'clsx';
 
 export const Input = ({ variant = "outline", ...props }: InputProps & { variant?: "outline" | "underline" }) => {
     return (
-        <HeadlessInput className={clsx(styles.input, {
-            [styles.outline]: variant === 'outline',
-            [styles.underline]: variant === 'underline',
-        })} {...props} />
+        <HeadlessInput className={clsx(styles.input, styles[variant])} {...props} />
     )
 };
