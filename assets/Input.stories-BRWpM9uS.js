945ad8500e49d2ff221a3714d4e372711f363abc
OlgaSpiridonova <127492063+OlgaSpiridonova@users.noreply.github.com>
2025-07-11 09:24:10 +0000

    Deploying to gh-pages from @ OlgaSpiridonova/ui-library@3c750358f6af45e0d62b443bbfb8a7ace87d2097 🚀

---
+++ assets/Input.stories-BRWpM9uS.js
@@ -0,0 +1,12 @@
+import{I as p}from"./Input-ihQ1Q7Df.js";import"./jsx-runtime-D_zvdyIk.js";import"./clsx-B-dksMZM.js";import"./render-D-CZJmXH.js";import"./index-D4lIrffr.js";const N={title:"Input",component:p},e={args:{name:"Name",placeholder:"Name"}},r={args:{name:"Name",placeholder:"Name",variant:"underline"}};var a,n,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    name: 'Name',
+    placeholder: 'Name'
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var t,m,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: 'Name',
+    placeholder: 'Name',
+    variant: 'underline'
+  }
+}`,...(s=(m=r.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};const g=["Outline","Underline"];export{e as Outline,r as Underline,g as __namedExportsOrder,N as default};
