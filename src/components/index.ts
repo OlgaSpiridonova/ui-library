b84277fb62a33919cdee249d2ba1737a0229f687
OlgaSpiridonova <o.spiridonova@tg.dunice.net>
2025-07-15 16:07:36 +0300

    fix: rename form field to text field

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { Button } from './Button';
 export { Input } from './Input';
 export { Textarea } from './Textarea';
-export { FormField } from './FormField';
+export { TextField } from './TextField';
 export { Link } from './Link';
