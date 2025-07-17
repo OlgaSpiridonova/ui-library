28385d921e88df800076bc5443dafba2423d8729
OlgaSpiridonova <127492063+OlgaSpiridonova@users.noreply.github.com>
2025-07-17 08:02:22 +0000

    Deploying to gh-pages from @ OlgaSpiridonova/ui-library@6eb342f99059c53c89e8d5c9c2aeb522e2e14223 🚀

---
+++ assets/Link.stories-DcMMpAAx.js
@@ -0,0 +1,10 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import{c as m}from"./clsx-B-dksMZM.js";import{F as l,I as p,T as u,Y as x}from"./YoutubeIcon-DM_kQccs.js";const I="_iconLink_z7oc0_1",j={iconLink:I},o=({iconLink:L=!1,...d})=>n.jsx("div",{className:m({[j.iconLink]:L}),children:n.jsx("a",{...d})});o.__docgenInfo={description:"",methods:[],displayName:"Link",props:{iconLink:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}}}};const T={title:"Link",component:o},e=()=>n.jsxs(n.Fragment,{children:[n.jsx(o,{iconLink:!0,children:n.jsx(l,{})}),n.jsx(o,{iconLink:!0,children:n.jsx(p,{})}),n.jsx(o,{iconLink:!0,children:n.jsx(u,{})}),n.jsx(o,{iconLink:!0,children:n.jsx(x,{})})]}),i={args:{children:"Text Link"}};e.__docgenInfo={description:"",methods:[],displayName:"IconLinks"};var s,c,r;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => <>
+    <Link iconLink><FacebookIcon /></Link>
+    <Link iconLink><InstagramIcon /></Link>
+    <Link iconLink><TiktokIcon /></Link>
+    <Link iconLink><YoutubeIcon /></Link>
+  </>`,...(r=(c=e.parameters)==null?void 0:c.docs)==null?void 0:r.source}}};var a,t,k;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Text Link'
+  }
+}`,...(k=(t=i.parameters)==null?void 0:t.docs)==null?void 0:k.source}}};const b=["IconLinks","TextLink"];export{e as IconLinks,i as TextLink,b as __namedExportsOrder,T as default};
